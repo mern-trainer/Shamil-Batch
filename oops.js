a2d90d5084eb7df669ca3b5bb2e348287aea81b0
VISHNU M K <futuralabs.mern@gmail.com>
2025-04-04 12:22:59 +0530

    oops basic

---
+++ oops.js
@@ -0,0 +1,48 @@
+// oops -> Object Oriented Programming System
+
+// object -> Collections of key value pairs
+
+// const obj = {
+//     name: "Shamil",
+//     age: 21,
+//     place: "Kozhikode"
+// }
+
+// obj.age = 23
+// obj.place = "Kannur"
+
+// delete obj.place
+
+// console.log(obj);
+
+// class -> Template or blueprint of an object.
+// object -> Instance of a class
+
+// constructor function -> invoke while creating an object
+// attributes -> same as variables
+// methods -> function
+// this -> rep. current object
+
+// PascalCase -> 
+class Person{
+
+    constructor(name) {
+        this.name = name
+    }
+
+    display(number) {
+        console.log("Display invoked " + number);
+    }
+
+    displayName() {
+        console.log(this.name);
+    }
+}
+
+const person = new Person("Shamil")
+
+person.display(10)
+
+person.displayName()
+
+console.log(person);
