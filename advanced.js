d08019b84bfc8f0573097c91584bf7c3d2daef1a
VISHNU M K <futuralabs.mern@gmail.com>
2025-04-07 12:34:52 +0530

    callback and mapping

--- advanced.js
+++ advanced.js
@@ -43,4 +43,59 @@ const sum = (firstNumber, secondNumber, callbackFunc) => {
     callbackFunc(result)
 }
 
-sum(10,100,displayResult)
+sum(10, 100, displayResult)
+
+// const handleLoop = (value) => {
+//     console.log(value);
+// }
+
+// const arrayLooping = () => {
+//     for (let i = 0; i < 10; i++) {
+//         handleLoop(i)
+//     }
+// }
+
+// arrayLooping()
+
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const arrayLooping = (cb) => {
+//     for (let i = 0; i < arr.length; i++) {
+//         cb(arr[i], i, arr)
+//     }
+// }
+
+// arrayLooping((element, index, array) => {
+//     console.log(element, index, array);
+// })
+
+// array method -> Map -> Array manipulation
+
+// length of array == length of result array
+
+const res = arr.map((element, index, array) => {
+    if (element % 2 == 0) {
+        return element * 10     
+    }
+    return element
+})
+
+console.log(res);
+
+const users = [
+    { name: "Shamil", age: 21 },
+    { name: "John", age: 22 },
+    { name: "Mark", age: 23 },
+    { name: "Jane", age: 24 },
+    { name: "Bob", age: 25 }
+]
+
+const res2 = users.map((user) => {
+    if (user.name == "John") {
+        user.age = 50
+    }
+    return user
+})
+
+console.log(res2);
